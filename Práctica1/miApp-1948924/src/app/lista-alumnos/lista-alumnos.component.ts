5c676f726d0f4132dc10acd5b295ef53afa1042d
Luna78453 <94145855+Luna78453@users.noreply.github.com>
2024-10-05 13:43:56 -0600

    Práctica 8: Action Sheet y Sliding Options

--- Práctica1/miApp-1948924/src/app/lista-alumnos/lista-alumnos.component.ts
+++ Práctica1/miApp-1948924/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NgFor } from '@angular/common';
-import { IonList, IonItem } from "@ionic/angular/standalone";
+import { IonList, IonItem, IonItemSliding, IonItemOptions, IonItemOption, IonActionSheet } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
-  imports:[NgFor, IonList, IonItem],
+  imports:[NgFor, IonList, IonItem, IonItemSliding, IonItemOptions, IonItemOption, IonActionSheet],
   standalone: true
 })
 export class ListaAlumnosComponent  implements OnInit {
@@ -17,4 +17,21 @@ export class ListaAlumnosComponent  implements OnInit {
   ngOnInit() {}
 
   alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+
+  public accionEliminar = [
+    {
+      text: 'Eliminar',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 }
