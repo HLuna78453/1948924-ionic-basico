419589d94633ae85885ad02da0219a42ce332e06
Luna78453 <94145855+Luna78453@users.noreply.github.com>
2024-10-05 13:13:37 -0600

    Práctica 7: Componentes (Re-hecho)

--- Práctica1/miApp-1948924/src/app/home/home.page.ts
+++ Práctica1/miApp-1948924/src/app/home/home.page.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
 import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component';
-import { IonHeader } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ListaAlumnosComponent],
 })
 export class HomePage {
-
   constructor() {}
-
 }
--- Práctica1/miApp-1948924/src/app/lista-alumnos/lista-alumnos.component.ts
+++ Práctica1/miApp-1948924/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+
 import { NgFor } from '@angular/common';
+import { IonList, IonItem } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
-  styleUrls: ['./lista-alumnos.component.scss']
+  styleUrls: ['./lista-alumnos.component.scss'],
+  imports:[NgFor, IonList, IonItem],
+  standalone: true
 })
 export class ListaAlumnosComponent  implements OnInit {
 
